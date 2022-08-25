e47b08f9c7c70ea1436d1536af699265593bef94
Baranidharan G <77617148+baranidharang@users.noreply.github.com>
2022-08-26 01:27:31 +0530

    Update speech.dart

--- git_final/lib/deaf/speech.dart
+++ git_final/lib/deaf/speech.dart
@@ -1,117 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'package:avatar_glow/avatar_glow.dart';
-import './speechTextSubmit.dart';
+import 'fifth_dupe.dart';
 
-class STT extends StatefulWidget {
-  const STT({key}) : super(key: key);
+// class FourthPage extends StatelessWidget {
+//   static const routeName = 'fourth_page';
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: MyHomePage(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//           appBar: AppBar(
+//         title: Text("Pic to Vibration", style: TextStyle(color: Colors.white)),
+//         shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.only(
+//                 bottomLeft: Radius.circular(25),
+//                 bottomRight: Radius.circular(25))),
+//         backgroundColor: Color.fromARGB(255, 119, 118, 118),
+//       )),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import './speech.dart';
+
+class Deaf extends StatefulWidget {
+  static const routeName = 'fourth_page';
+  const Deaf({Key? key}) : super(key: key);
 
   @override
-  State<STT> createState() => _STT();
+  State<Deaf> createState() => _DeafState();
 }
 
-class _STT extends State<STT> {
-  late stt.SpeechToText _speech;
-  bool _isListening = false;
-  String _textSpeech = "Press the button and start speaking";
-  int buttonPressedCount = 0;
-
-  void onListen() async {
-    if (!_isListening) {
-      bool available = await _speech.initialize(
-        onStatus: (val) => print('onStatus:$val'),
-        onError: (val) => print('onError:$val'),
-      );
-      if (available) {
-        setState(() {
-          _isListening = true;
-        });
-        _speech.listen(
-            onResult: (val) => setState(() {
-                  _textSpeech = val.recognizedWords;
-                }));
-      }
-    } else {
-      setState(() {
-        _isListening = false;
-        _speech.stop();
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _speech = stt.SpeechToText();
-  }
-
+class _DeafState extends State<Deaf> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Learn Square"),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: AvatarGlow(
-          animate: _isListening,
-          glowColor: Colors.blue,
-          endRadius: 90.0,
-          duration: const Duration(milliseconds: 2000),
-          repeat: true,
-          showTwoGlows: true,
-          repeatPauseDuration: Duration(milliseconds: 100),
-          child: FloatingActionButton(
-            child: Icon(
-              _isListening ? Icons.mic : Icons.mic_none,
-              size: 36,
-            ),
+    final ButtonStyle = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+    
+      return Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton.icon(
             onPressed: () {
-              onListen();
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) =>STT()),
+              );
             },
+            icon:const Icon(
+              Icons.mic_rounded,
+            ),
+            label:const Text('SPEAK'),
           ),
-        ),
-        body: SingleChildScrollView(
-          child: Card(
-              margin: EdgeInsets.all(10),
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: 100,
-                ),
-                child: Column(children: [
-                  ListTile(
-                    leading: Icon(Icons.message),
-                    title: Text(
-                      _textSpeech,
-                      style:
-                         const  TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text('SUBMIT'),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    SpeechTextSubmit(_textSpeech)),
-                          );
-                        },
-                      ),
-                      const SizedBox(width: 8),
-                      TextButton(
-                        child: const Text('GO BACK'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                ]),
-              )),
-        ));
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ));
+   
   }
 }
